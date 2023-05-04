a6302a8d78c7b187cb22fc4d8617e69d7c9125a3
Dmitriy <d.krumkachev@gmail.com>
2023-05-04 18:14:43 +0300

    Добавьте файлы проекта.

---
+++ Server/Server.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Server
+    {
+        private struct Client
+        {
+            public Client(Socket socket, string name)
+            {
+                Socket = socket;
+                Name = name;
+            }
+
+            public Socket Socket { get; }
+
+            public string Name { get; set; }
+        }
+
+        private readonly List<Client> clients = new();
+        private const int ChunkSize = 1024;
+        private 
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
